46d480332dec9314f49bd56c8f6cd2873d0e1094
FranciscoETSD <tadeo00704@gmail.com>
2021-10-13 08:48:42 -0400

    Comparar vectores con recursividad

--- src/main.java
+++ src/main.java
@@ -21,13 +21,37 @@ public class main {
             System.out.println("Se realizo la  inversion con exito ...");
         }
 
-        private static String invertirCadena(String cadena){
-            if(cadena.length()==1){
+        private static String invertirCadena(String cadena) {
+            if (cadena.length() == 1) {
                 return cadena;
             } else {
                 return invertirCadena(cadena.substring(1)) + cadena.charAt(0);
 
             }
         }
+    }
+
+    /*
+    Metodo recursivo para comparardos vectores de enteros elemento por elementi¿o
+    vector1 = {5, 6, 2}
+    vector2 = {2, 9, 2}
+    diferen = 3 + -3 + 0 = 0 = 6
+     */
+    private static int compararVectores(int[] vector1, int[] vector2, int indice) {
+        int sumaDiferencias;
+
+        if (indice == vector1.length - 1) { // caso base
+            sumaDiferencias = Math.abs(vector1[indice] - vector2[indice]);
+        } else { //llamada recursiva
+            sumaDiferencias = Math.abs(vector1[indice] - vector2[indice]) + compararVectores(vector1, vector2, indice + 1);
+        }
+
+        return sumaDiferencias;
+    }
+    /*
+    Metodo auxiliar que realiza la llamada al metodo recursivo
+     */
+    public static boolean compararVectores(int[] vector1, int[] vector2) {
+        return compararVectores(vector1,vector2,0) == 0;
     }
 }
---
+++ src/main2.java
@@ -0,0 +1,7 @@
+public class main2 {
+    public static void main(String[] args) {
+        int[]vector1 = {5, 6, 2};
+        int[]vector2 = {2, 6, 8};
+        System.out.println(main.compararVectores(vector1,vector2));
+    }
+}
